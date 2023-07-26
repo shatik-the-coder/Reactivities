6d69df7f2f55d8788a5ef6f60edeaa89f06e18f2
shatik-the-coder <shatik.the.coder@gmail.com>
2023-07-26 19:23:59 +0300

    Typescript demo learn TS features

--- client-app/src/demo.ts
+++ client-app/src/demo.ts
@@ -1,1 +1,19 @@
-let data = 42;
+export interface Duck {
+    name: string;
+    numLegs: number;
+    makeSound: (sound : string) => void;
+}
+
+const duck1: Duck = {
+    name: "huey",
+    numLegs: 2,
+    makeSound: (sound) => console.log(sound)
+}
+
+const duck2: Duck = {
+    name: "dewey",
+    numLegs: 2,
+    makeSound: (sound) => console.log(sound)
+}
+
+export const ducks = [duck1, duck2];
